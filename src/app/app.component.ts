d03eff8c24715d885f076f9c9fb6dafc0e4a5717
Jackie Jin <liveaeon@gmail.com>
2019-11-01 00:10:56 -0700

    online code editor deployed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ACLCWebDIY';
+export class AppComponent implements AfterViewInit {
+  name = 'ACLC Web DIY Code Editor';
+  @ViewChild('editor', {static: false}) editor;
+
+  ngAfterViewInit() {
+
+    this.editor.getEditor().setOptions({
+      showLineNumbers: true,
+      tabSize: 2
+    });
+
+    this.editor.mode = 'javascript';
+    this.editor.value = `function testThis() {
+  console.log("Sup, ACLC!")
+}`
+
+    this.editor.getEditor().commands.addCommand({
+      name: "showOtherCompletions",
+      bindKey: "Ctrl-.",
+      exec: function (editor) {
+
+      }
+    })
+  }
 }
