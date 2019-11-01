b254fd10d22c28c2a4fd10bc571ae695cde97432
Jackie Jin <liveaeon@gmail.com>
2019-10-31 23:48:00 -0700

    server path fix

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const path = require('path');
 const express = require('express');
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/ACLCWebDIY'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/ACLCWebDIY/index.html'));
+});
+
+// default Heroku port
 app.listen(process.env.PORT || 8080);
